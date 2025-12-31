c6feda58ba96b689000b294ab851052be6dc7449
Rohan Aditya <adityathirumala20@gmail.com>
2025-12-31 12:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ setupCarousel({
     dots: ".plans-dots"
 });
 
-//document.addEventListener('contextmenu', (event) => event.preventDefault());
+document.addEventListener('contextmenu', (event) => event.preventDefault());
 
 document.addEventListener('dragstart', event => {
     if (event.target.tagName === 'IMG') {
